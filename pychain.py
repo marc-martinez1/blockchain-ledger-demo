ad3532b7b93971f3cfb53d4e589d7bef50446188
Marc Martinez <135652230+marc-martinez1@users.noreply.github.com>
2023-10-23 18:24:14 -0700

    commit python code finalization

--- pychain.py
+++ pychain.py
@@ -72,8 +72,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    record: List[Record]
-
+    record: Record
     creator_id: int
     prev_hash: str = "0"
     timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
@@ -193,7 +192,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        record=Record,
+        record=Record(sender,receiver,amount),
         creator_id=42,
         prev_hash=prev_block_hash
     )
